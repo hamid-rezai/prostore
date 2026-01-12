46b4889ef52a0a27bbda6b731a18574228cc92dd
Hamid Rezai <hrezai067@gmail.com>
2026-01-13 10:20:44 +1100

    fix prisma neon adapter + auth core dedupe

--- db/prisma.ts
+++ db/prisma.ts
@@ -8,10 +8,10 @@ neonConfig.webSocketConstructor = ws;
 const connectionString = `${process.env.DATABASE_URL}`;
 
 // Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
-const pool = new Pool({ connectionString });
+const poolConfig = { connectionString };
 
 // Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
-const adapter = new PrismaNeon(pool);
+const adapter = new PrismaNeon(poolConfig);
 
 // Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
 export const prisma = new PrismaClient({ adapter }).$extends({
